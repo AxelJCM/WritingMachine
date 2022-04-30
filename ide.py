82effd437cc2917652ff4f1fe28ab4d9b9070fb8
Mario <48070830+mjcarranza@users.noreply.github.com>
2022-04-29 22:32:42 -0600

    Update ide.py

--- ide.py
+++ ide.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import font
 from tkinter import messagebox
+from idlelib.colorizer import ColorDelegator
+from idlelib.percolator import Percolator
+
+from archivos import leerArchivo
 
 #Variables
 fuente = "Consolas"
@@ -16,7 +20,7 @@ def texto(pantalla, mensaje, x, y, color, colorFondo = None):
 
 # Creacion de la ventana del ide
 ide = Tk()
-ide.title('Writting Machine IDE')
+ide.title('Writing Machine IDE')
 ide.iconbitmap('C:\\Users\\mario\\Desktop\\GitHub\\WritingMachine\\Imagenes\\icono.ico')
 ide.geometry("1000x700")
 ide.resizable(False, False)
@@ -26,30 +30,38 @@ ide.configure(background='black')
 baseIde = Canvas(ide, width = 900, height= 545)
 baseIde.place(x=0, y=0)
 areaTexto = Text(baseIde, height=34, width=112)
+areaTexto.config(bg='#362f2e', fg='#d2ded1')
+Percolator(areaTexto).insertfilter(ColorDelegator()) # Hacer algo como esto para cuando se reconozca una palabra
 areaTexto.pack()
 # Area donde se muestran los mensajes de compilador
 baseConsola = Canvas(ide, width=490, height=143)
 baseConsola.place(x=0, y=550)
 areaConsola = Text(baseConsola, height=9, width=60)
+areaConsola.config(bg='#362f2e', fg='#1dd604')
 areaConsola.pack()
 # Area donde se muestran los mensajes impresos por parte del codigo
 basePrint = Canvas(ide, width=490, height=143)
 basePrint.place(x=510, y=550)
 areaPrint = Text(basePrint, height=9, width=60)
+areaPrint.config(bg='#362f2e', fg='#d2ded1')
 areaPrint.pack()
-
-#### PROGRAMAR LAS ACCIONES PARA LOS BOTONES AQUI #########################################################
 
 # Area donde se muestran los botones para compilar, corrrer el codigo ....
 baseCompi = Canvas(ide, width=100, height=545)
 baseCompi.place(x=905, y=2)
+# Botones del ide
+nuevoBtn = Button(baseCompi, text ="   NUEVO   ", activebackground='light blue', activeforeground='red')
+nuevoBtn.pack(ipadx=10, ipady=0, expand=True)
+
 abrirBtn = Button(baseCompi, text ="    ABRIR     ", activebackground='light blue', activeforeground='red')
 abrirBtn.pack(ipadx=10, ipady=0, expand=True)
 
-### Boton "NUEVO"  ####
+guardarBtn = Button(baseCompi, text ="GUARDAR ", activebackground='light blue', activeforeground='red')
+guardarBtn.pack(ipadx=10, ipady=0, expand=True)
 
 compilarBtn = Button(baseCompi, text ="COMPILAR", activebackground='light blue', activeforeground='red')
 compilarBtn.pack(ipadx=10, ipady=0, expand=True)
+
 ejecutarBtn = Button(baseCompi, text =" EJECUTAR ", activebackground='light blue', activeforeground='red')
 ejecutarBtn.pack(ipadx=10, ipady=0, expand=True)
 
